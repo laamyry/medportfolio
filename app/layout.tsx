2db37902dec3ed50fba814639d05d9a704fff40e
laamyry <slamiri10@gmail.com>
2024-08-14 00:57:44 +0100

    chore: Add @vercel/analytics package for website analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "Med Laamyry Portfolio",
   description: "A professional portfolio website for Med Laamyry",
@@ -9,7 +9,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <main className=" mx-auto ">{children}</main>
+        <main className=" mx-auto ">
+          {children}
+          <Analytics />
+        </main>
       </body>
     </html>
   );
