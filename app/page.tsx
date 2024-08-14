0f5f48d70e8b8a2c520c1f9383065313e8e0773c
laamyry <slamiri10@gmail.com>
2024-08-14 23:50:17 +0100

    chore: Add Clients component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Works from '../components/Works';
 import Contact from '../components/contact';
 import Footer from '../components/footer';
 import Navbar from '../components/Navbar';
+import Clinets from '../components/clinets';
 export default function Home() {
   return (
     <>
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Works />
       <Contact /> 
+      <Clinets />
       <Footer />
     </>
   );
