93deb81fa9988013da44614d5237eb2b042ddc89
laamyry <slamiri10@gmail.com>
2024-08-18 16:18:07 +0100

    chore: Update Sitemap component with correct URLs and metadata

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,31 +4,31 @@ import { MetadataRoute } from "next";
 export default function Sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://medlaamyry.com/",
+      url: "https://www.medlaamyry.com/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://medlaamyry.com/about",
+      url: "https://www.medlaamyry.com/about",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://medlaamyry.com/works",
+      url: "https://www.medlaamyry.com/works",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
     },
     {
-      url: "https://medlaamyry.com/contact",
+      url: "https://www.medlaamyry.com/contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://medlaamyry.com/clients",
+      url: "https://www.medlaamyry.com//clients",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
